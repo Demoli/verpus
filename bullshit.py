1c8899b505bcbbc051e0aa718006f774918ca489
Neil Aitken <demoli@users.noreply.github.com>
2018-02-23 22:34:02 +0000

    Make bullshit limit configurable

--- bullshit.py
+++ bullshit.py
@@ -1,8 +1,14 @@
+import json
+
 class Bullshit:
     def __init__(self):
         self.bullshitWords = lines = open("bullshit.txt").read().lower().splitlines()
         self.detectedBullshit = 0
-        self.bullshitLimit = 2
+
+        with open("config.json") as file:
+            config = json.load(file)
+
+        self.bullshitLimit = config["bullshit_limit"]
 
     def detect(self, words):
         words = words.split(" ");
